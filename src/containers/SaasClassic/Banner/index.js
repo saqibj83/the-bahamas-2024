e5bb2956ff8078dd43a4f411ff6e1dd498fab226
Muhammad Saqib <saqib2uk@hotmail.com>
2023-06-12 07:16:40 +0500

    video-date

--- src/containers/SaasClassic/Banner/index.js
+++ src/containers/SaasClassic/Banner/index.js
@@ -65,26 +65,26 @@ const BannerSection = ({ row, contentWrapper, title, imageWrapper }) => {
             />
           </Box>
           <div className="video-section">
-            {/* <Fade bottom>
+            <Fade bottom>
               <ReactPlayer
-                url="https://www.youtube.com/embed/-CQ7dEEIh28"
+                url="https://www.youtube.com/embed/weABIBrDg7c"
                 // light="https://cdn.triton-series.com/wp-content/uploads/2019/12/29120652/video-banner.png"
                 playing
                 width="711px"
                 height="400px"
                 controls
               />
-            </Fade> */}
-            <NextImage
+            </Fade>
+            {/* <NextImage
               src={Video}
               alt="Video image"
               style={{ display: "inline" }}
-            />
+            /> */}
           </div>
           <Box {...contentWrapper}>
             <div className="text-st">
               JW MARRIOTT GROSVENOR HOUSE <br />
-              THURSDAY 27th JULY to THURSDAY 10th AUGUST
+              July 27th - Aug 10th, 2023
             </div>
           </Box>
         </Box>
