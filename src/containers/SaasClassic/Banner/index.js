4c30614ca7dc19069e0e406c3ee87bc8e1517163
Muhammad Saqib <saqib2uk@hotmail.com>
2023-08-02 17:13:19 +0500

    youtube

--- src/containers/SaasClassic/Banner/index.js
+++ src/containers/SaasClassic/Banner/index.js
@@ -25,7 +25,7 @@ const ModalContent = () => (
   <VideoWrapper>
     <iframe
       title="Video"
-      src="https://www.youtube.com/embed/Ij3qoud9BwM"
+      src="https://www.youtube.com/embed/3kSgCdsHNBA"
       frameBorder="0"
     />
   </VideoWrapper>
@@ -67,7 +67,7 @@ const BannerSection = ({ row, contentWrapper, title, imageWrapper }) => {
           <div className="video-section">
             <Fade bottom>
               <ReactPlayer
-                url="https://www.youtube.com/embed/Ij3qoud9BwM"
+                url="https://www.youtube.com/embed/3kSgCdsHNBA"
                 // light="https://cdn.triton-series.com/wp-content/uploads/2019/12/29120652/video-banner.png"
                 playing={true}
                 playsinline
