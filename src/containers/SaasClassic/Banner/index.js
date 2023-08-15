16f9f8acc09b78de9445c7e0e4ce36e7f25d1652
Muhammad Saqib <saqib2uk@hotmail.com>
2023-08-15 15:24:39 +0500

    revert-back

--- src/containers/SaasClassic/Banner/index.js
+++ src/containers/SaasClassic/Banner/index.js
@@ -25,7 +25,7 @@ const ModalContent = () => (
   <VideoWrapper>
     <iframe
       title="Video"
-      src="https://www.youtube.com/embed/XggREF7hlQk"
+      src="https://www.youtube.com/embed/q-qYjg2POxk"
       frameBorder="0"
     />
   </VideoWrapper>
@@ -67,7 +67,7 @@ const BannerSection = ({ row, contentWrapper, title, imageWrapper }) => {
           <div className="video-section">
             <Fade bottom>
               <ReactPlayer
-                url="https://www.youtube.com/embed/XggREF7hlQk"
+                url="https://www.youtube.com/embed/q-qYjg2POxk"
                 // light="https://cdn.triton-series.com/wp-content/uploads/2019/12/29120652/video-banner.png"
                 playing={true}
                 playsinline
@@ -84,7 +84,9 @@ const BannerSection = ({ row, contentWrapper, title, imageWrapper }) => {
           </div>
           <Box {...contentWrapper}>
             <div className="text-st">
-              <span>AUGUST 11<sup>th</sup>, 2023</span> 
+              <span>
+                AUGUST 11<sup>th</sup>, 2023
+              </span>
             </div>
           </Box>
         </Box>
