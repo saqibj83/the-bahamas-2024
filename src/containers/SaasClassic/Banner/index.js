54779a0129b43fd34c4cb432ee04e623f82babba
saqibj83 <98015804+saqibj83@users.noreply.github.com>
2023-11-04 20:39:18 +0800

    vod

--- src/containers/SaasClassic/Banner/index.js
+++ src/containers/SaasClassic/Banner/index.js
@@ -25,7 +25,7 @@ const ModalContent = () => (
   <VideoWrapper>
     <iframe
       title="Video"
-      src="https://www.youtube.com/embed/GREah8j1CFE"
+      src="https://www.youtube.com/embed/73696-s9t0ih6y"
       frameBorder="0"
     />
   </VideoWrapper>
@@ -67,7 +67,7 @@ const BannerSection = ({ row, contentWrapper, title, imageWrapper }) => {
           <div className="video-section">
             <Fade bottom>
               <ReactPlayer
-                url="https://www.youtube.com/embed/GREah8j1CFE"
+                url="https://www.youtube.com/embed/73696-s9t0ih6y"
                 // light="https://cdn.triton-series.com/wp-content/uploads/2019/12/29120652/video-banner.png"
                 playing={true}
                 playsinline
