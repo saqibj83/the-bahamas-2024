815820d0507c8702e58d0ccec9f99a6be11aacf1
saqibj83 <98015804+saqibj83@users.noreply.github.com>
2024-09-02 17:39:59 +0800

    cover

--- src/containers/SaasClassic/Banner/banner.style.js
+++ src/containers/SaasClassic/Banner/banner.style.js
@@ -18,12 +18,12 @@ const Bubble = keyframes`
 export const BannerWrapper = styled.section`
   padding: 100px 0 50px 0;
   background-image: url("https://cdn.triton-series.com/wp-content/uploads/2019/12/02151737/web-cover.png");
-  background-size: contain;
+  background-size: cover;
   background-position: top center;
   overflow: hidden;
   background-repeat: no-repeat;
   background-color: #000;
-  min-height: 90vh;
+  min-height: 100vh;
   .video-section {
     width: 100%;
     display: flex;
