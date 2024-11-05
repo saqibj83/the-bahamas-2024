55ac2fc207eaab65edc992cde54536f7b72efa38
saqibj83 <98015804+saqibj83@users.noreply.github.com>
2024-11-06 00:28:34 +0800

    VOD-5

--- src/containers/SaasClassic/Banner/index.js
+++ src/containers/SaasClassic/Banner/index.js
@@ -59,7 +59,7 @@ const BannerSection = ({ row, contentWrapper, title, imageWrapper }) => {
             <div className="video-section">
               <Fade bottom>
                 <ReactPlayer
-                  url="https://www.youtube.com/embed/d3A8niCIeQo"
+                  url="https://www.youtube.com/embed/p1-gCymak6Q"
                   playing={true}
                   playsinline
                   width="711px"
