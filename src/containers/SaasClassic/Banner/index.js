17b73b5adc038a7d416fa3f7814375dbac0341e9
saqibj83 <98015804+saqibj83@users.noreply.github.com>
2024-12-09 10:34:56 +0800

    FOD-2

--- src/containers/SaasClassic/Banner/index.js
+++ src/containers/SaasClassic/Banner/index.js
@@ -23,7 +23,7 @@ const ModalContent = () => (
   <VideoWrapper>
     <iframe
       title="Video"
-      src="https://www.youtube.com/embed/YZEDolAvOo4"
+      src="https://www.youtube.com/embed/Hh-9Vm7eIWA"
       frameBorder="0"
     />
   </VideoWrapper>
@@ -63,7 +63,7 @@ const BannerSection = ({ row, contentWrapper, title, imageWrapper }) => {
             <div className="video-section">
               <Fade bottom>
                 <ReactPlayer
-                  url="https://www.youtube.com/embed/YZEDolAvOo4"
+                  url="https://www.youtube.com/embed/Hh-9Vm7eIWA"
                   playing={true}
                   playsinline
                   width="711px"
